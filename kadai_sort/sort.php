b2f84e0f5202ff3d687cd32f64179567988463e1
K56az <icr56az@gmail.com>
2025-05-05 14:00:46 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,33 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <title>PHP基礎編</title>
-</head>
-
-<body>
-    <p>
-        <?php
-        $nums=[15,4,18,23,10];
-        function soft_2way($array,$order){
-            if($order==TRUE){
-                sort($array);
-                echo "昇順にソートします。<br>";
-                foreach($array as $array_element){
-                    echo "$array_element<br>";
-                }
-            } else{
-                rsort($array);
-                echo "降順にソートします。<br>";
-                foreach($array as $array_element){
-                    echo "$array_element<br>";
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP基礎編</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            $nums=[15,4,18,23,10];
+            function sort_2way($array,$order){
+                if($order==TRUE){
+                    sort($array);
+                    echo "昇順にソートします。<br>";
+                    foreach($array as $array_element){
+                        echo "$array_element<br>";
+                    }
+                } else{
+                    rsort($array);
+                    echo "降順にソートします。<br>";
+                    foreach($array as $array_element){
+                        echo "$array_element<br>";
+                    }
                 }
             }
-        }
-        soft_2way($nums,TRUE);
-        ?>
-    </p>
-</body>
-
+            sort_2way($nums,TRUE);
+            ?>
+        </p>
+    </body>
 </html>
